bc53ac3ca13022507c54984410815a6f0b34d615
Yuqing Ai <yuqingai@emeraldinno.com>
2025-07-09 07:47:27 -0400

    Using the same architecture and details in the paper.

--- config.py
+++ config.py
@@ -2,19 +2,22 @@ import torch
 
 config = {
     "device": "cuda" if torch.cuda.is_available() else "cpu",
-    "dataset": "MNIST", # Options: "MNIST", "CIFAR10"
-    "experiment_id": "exp_008",
+    "dataset": "CIFAR10", # Options: "MNIST", "CIFAR10"
+    "experiment_id": "exp_009",
     "image_size": 32,
-    "channel_multiplier": 0.375, 
+    "model_name": "Unet", # Options: "MobileResUNet", "Unet"
+    "channel_multiplier": 1.0, 
     "batch_size": 64,
     "epochs_stage1": 75,
     "epochs_stage2": 50,
-    "lr": 3e-3,
+    "lr": 2e-4,
+    "dropout_rate": 0.15,
+    "ema_decay": 0.999999,
     "n_images_to_generate": 36,
     "generation_steps": 100, 
     "checkpoint_interval": 5,
     "fid_num_images": 2000,
     "fid_stats_dir": "data/fid_stats", # Directory to save/load FID statistics
     "calculate_fid": False,
-    "load_model_path": None #"/home/yuqing/rectified_flow/experiments/CIFAR10/exp_007/models/stage1_epoch_75.pth", # Path to a model checkpoint to load (e.g., "/path/to/model.pth")
+    "load_model_path": None
 }
--- train.py
+++ train.py
@@ -7,21 +7,23 @@ import os
 from tqdm import tqdm
 import numpy as np
 import json
+from ema_pytorch import EMA
 
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 from config import config
-from models import MobileResUNet
+from models import Unet
 from utils import InceptionV3, calculate_fid_stats, calculate_fid_for_model, generate_images, show_images
 
 
-def train_one_epoch(model, dataloader, optimizer, loss_fn, device):
+def train_one_epoch(model, ema, dataloader, optimizer, loss_fn, device):
     model.train()
     total_loss = 0
     pbar = tqdm(dataloader, desc="Training Stage 1")
     beta_dist = torch.distributions.beta.Beta(0.5, 0.5)
     for z1, _ in pbar:
-        z1, z0 = z1.to(device), torch.randn_like(z1)
+        z1 = z1.to(device)
+        z0 = torch.randn_like(z1)
         t_scalar = beta_dist.sample((z0.shape[0],)).to(device)
         t = t_scalar.view(-1, 1, 1, 1)
         zt = (1 - t) * z0 + t * z1
@@ -31,13 +33,14 @@ def train_one_epoch(model, dataloader, optimizer, loss_fn, device):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        ema.update()
         total_loss += loss.item()
         pbar.set_postfix({"Loss": loss.item()})
     return total_loss / len(dataloader)
 
 
 if __name__ == "__main__":
-    base_experiment_dir = f"/home/yuqing/rectified_flow/experiments/{config['dataset']}/{config['experiment_id']}"
+    base_experiment_dir = f"experiments/{config['dataset']}/{config['experiment_id']}"
     models_save_dir = os.path.join(base_experiment_dir, "models")
     images_save_dir = os.path.join(base_experiment_dir, "images")
     configs_save_dir = os.path.join(base_experiment_dir, "configs")
@@ -84,28 +87,41 @@ if __name__ == "__main__":
             np.savez(fid_stats_path, mu_real=mu_real, sigma_real=sigma_real)
             print(f"Done. Statistics saved to {fid_stats_path}")
 
-    model_1 = MobileResUNet(config['in_channels'], config['out_channels'], channel_multiplier=config['channel_multiplier'], num_groups=4).to(config['device'])
+    if config["model_name"] == "Unet":
+        model_1 = Unet(
+            dim = 64,
+            dim_mults = (1, 2, 4, 8),
+            channels = config['in_channels'],
+            out_dim = config['out_channels'],
+            dropout = config['dropout_rate']
+        ).to(config['device'])
+    else:
+        raise ValueError(f"Unknown model name: {config['model_name']}")
+
     optimizer_1 = torch.optim.Adam(model_1.parameters(), lr=config['lr'])
     scheduler_1 = ReduceLROnPlateau(optimizer_1, mode='min', factor=0.5, patience=5)
     loss_fn = nn.MSELoss()
+    ema = EMA(model_1, beta = config['ema_decay'], update_every = 10)
+    ema.to(config['device'])
 
     if config["load_model_path"]:
         print(f"Loading model from {config['load_model_path']}...")
         checkpoint = torch.load(config['load_model_path'], map_location=config['device'])
         model_1.load_state_dict(checkpoint['model_state_dict'])
+        ema.load_state_dict(checkpoint['ema_state_dict'])
         print("Model loaded successfully.")
 
     print("--- Stage 1: Training 1-Rectified Flow Model ---")
     for epoch in range(config['epochs_stage1']):
-        avg_loss = train_one_epoch(model_1, dataloader, optimizer_1, loss_fn, config['device'])
+        avg_loss = train_one_epoch(model_1, ema, dataloader, optimizer_1, loss_fn, config['device'])
         print(f"Stage 1 | Epoch {epoch+1}/{config['epochs_stage1']}, Loss: {avg_loss:.4f}")
         scheduler_1.step(avg_loss)
-        vis_images = generate_images(model_1, config['n_images_to_generate'], config['generation_steps'], config['device'])
+        vis_images = generate_images(ema.ema_model, config['n_images_to_generate'], config['generation_steps'], config['device'])
         img_title = f"Stage 1, Epoch {epoch+1}"
         if (epoch + 1) % config['checkpoint_interval'] == 0:
-            torch.save({'model_state_dict': model_1.state_dict()}, f"{models_save_dir}/stage1_epoch_{epoch+1}.pth")
+            torch.save({'model_state_dict': model_1.state_dict(), 'ema_state_dict': ema.state_dict()}, f"{models_save_dir}/stage1_epoch_{epoch+1}.pth")
             if config["calculate_fid"]:
-                fid_score = calculate_fid_for_model(model_1, mu_real, sigma_real, inception_model, config['device'])
+                fid_score = calculate_fid_for_model(ema.ema_model, mu_real, sigma_real, inception_model, config['device'])
                 print(f"FID Score after {epoch+1} epochs: {fid_score:.2f}")
                 img_title += f"\nFID: {fid_score:.2f}"
         show_images(vis_images, img_title, f"{images_save_dir}/stage1_epoch_{epoch+1}.png")
@@ -114,38 +130,58 @@ if __name__ == "__main__":
     model_1.eval()
 
     print("\n--- Stage 2: Training 2-Rectified Flow Model (Reflow) ---")
-    model_2 = MobileResUNet(config['in_channels'], config['out_channels'], channel_multiplier=config['channel_multiplier'], num_groups=4).to(config['device'])
+    if config["model_name"] == "Unet":
+        model_2 = Unet(
+            dim = 64,
+            dim_mults = (1, 2, 4, 8),
+            channels = config['in_channels'],
+            out_dim = config['out_channels'],
+            dropout = config['dropout_rate']
+        ).to(config['device'])
+    else:
+        raise ValueError(f"Unknown model name: {config['model_name']}")
+
     optimizer_2 = torch.optim.Adam(model_2.parameters(), lr=config['lr'])
     scheduler_2 = ReduceLROnPlateau(optimizer_2, mode='min', factor=0.5, patience=5)
+    ema_2 = EMA(model_2, beta = config['ema_decay'], update_every = 10)
+    ema_2.to(config['device'])
     beta_dist = torch.distributions.beta.Beta(0.5, 0.5)
     
     for epoch in range(config['epochs_stage2']):
         model_2.train()
         total_loss = 0
-        pbar = tqdm(range(len(dataloader)), desc=f"Training Stage 2, Epoch {epoch+1}")
-        for _ in pbar:
-            z0 = torch.randn(config['batch_size'], config['in_channels'], config['image_size'], config['image_size'], device=config['device'])
-            z1 = generate_images(model_1, config['batch_size'], config['generation_steps'], config['device'], start_noise=z0)
-            t = beta_dist.sample((z0.shape[0],)).to(config['device']).view(-1, 1, 1, 1)
+        pbar = tqdm(dataloader, desc=f"Training Stage 2, Epoch {epoch+1}")
+        for z1, _ in pbar:
+            optimizer_2.zero_grad()
+
+            z1 = z1.to(config['device'])
+            z0 = torch.randn_like(z1)
+
+            t_scalar = beta_dist.sample((z0.shape[0],)).to(config['device'])
+            t = t_scalar.view(-1, 1, 1, 1)
+
             zt = (1 - t) * z0 + t * z1
             target = z1 - z0
-            pred = model_2(zt, t.squeeze())
+
+            pred = model_2(zt, t_scalar)
             loss = loss_fn(pred, target)
-            optimizer_2.zero_grad()
+
             loss.backward()
             optimizer_2.step()
+            ema_2.update()
+
             total_loss += loss.item()
             pbar.set_postfix({"Loss": loss.item()})
         
         avg_loss = total_loss / len(dataloader)
         print(f"Stage 2 | Epoch {epoch+1}/{config['epochs_stage2']}, Loss: {avg_loss:.4f}")
         scheduler_2.step(avg_loss)
-        vis_images = generate_images(model_2, config['n_images_to_generate'], config['generation_steps'], config['device'])
+        vis_images = generate_images(ema_2.ema_model, config['n_images_to_generate'], config['generation_steps'], config['device'])
         img_title = f"Stage 2, Epoch {epoch+1}"
         if (epoch + 1) % config['checkpoint_interval'] == 0:
-            torch.save({'model_state_dict': model_2.state_dict()}, f"{models_save_dir}/stage2_epoch_{epoch+1}.pth")
+            torch.save({'model_state_dict': model_2.state_dict(), 'ema_state_dict': ema_2.state_dict()}, f"{models_save_dir}/stage2_epoch_{epoch+1}.pth")
             if config["calculate_fid"]:
-                fid_score = calculate_fid_for_model(model_2, mu_real, sigma_real, inception_model, config['device'])
+                fid_score = calculate_fid_for_model(ema_2.ema_model, mu_real, sigma_real, inception_model, config['device'])
                 print(f"FID Score after {epoch+1} epochs: {fid_score:.2f}")
                 img_title += f"\nFID: {fid_score:.2f}"
         show_images(vis_images, img_title, f"{images_save_dir}/stage2_epoch_{epoch+1}.png")
