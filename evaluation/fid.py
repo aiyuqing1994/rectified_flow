Fix an issue on FID score calculation.

evaluation/fid.py
@@ -73,7 +73,7 @@ def calculate_fid_for_model(gen_model, mu_real, sigma_real, inception_model, con
         if stage == 3:
             imgs = gen_model(torch.randn(n, config['in_channels'], config['image_size'], config['image_size'], device=device))
         else:
-            imgs = generate_images(gen_model, config, device)
+            imgs = generate_images(gen_model, config, device, num_samples=n)
         
         fake_images.append(imgs.cpu().numpy())
         num_images_generated += imgs.shape[0]

generation/generators.py
@@ -4,11 +4,18 @@ import torch
 
 
 @torch.no_grad()
-def generate_images(model, config, device, start_noise=None, steps=None):
+def generate_images(model, config, device, start_noise=None, steps=None, num_samples=None):
     model.eval()
     if steps is None:
         steps = config['generation_steps']
-    z = start_noise if start_noise is not None else torch.randn(config.get('n_images_to_generate', 36), config['in_channels'], config['image_size'], config['image_size'], device=device)
+    
+    if start_noise is not None:
+        z = start_noise
+    else:
+        if num_samples is None:
+            num_samples = config.get('n_images_to_generate', 36)
+        z = torch.randn(num_samples, config['in_channels'], config['image_size'], config['image_size'], device=device)
+    
     dt = 1.0 / steps
     for i in range(steps):
         t = torch.full((z.shape[0],), i / steps, device=device)